c4adc43b8a738d7f92a1b0976b4f61fcda480b4d
IvanKorolev <Ivankorol1512@yandex.ru>
2026-04-06 18:53:03 +0300

    add ClientAVL h

--- headers/structures/ClientAVL.h
+++ headers/structures/ClientAVL.h
@@ -1,15 +1,36 @@
 #pragma once
 
 #include <string>
-// тут должен быть include Client
+#include "Client.h"
+
+
+
 struct AVLNode
 {
-    int passport_number;
-    // Client client;
+    long long passport_number;
+    Client client;
     int height;
 
     AVLNode* Prev;
     AVLNode* Left;
     AVLNode* Right;
 
+
+    void refreshHeight();
+    int GetHeight();
+    void DeleteTree();
+    void LeftRotate();
+    void RightRotate();
+    void BigLeftRotate();
+    void BigRightRotate();
+    void BalanceTree(AVLNode*&);
+
+
+    void AddClient(Client);
+    void deleteClient(long long);
+    void showAllClients();
+    void deleteAllClients();
+    void findClientByPassport(long long);
+    void findClientByFio(std::string);
+    void findClientByAdress(std::string);
 };

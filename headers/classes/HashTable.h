c9958b9ec7417029feb4a44b3327481cad385efd
IvanKorolev <Ivankorol1512@yandex.ru>
2026-04-15 19:18:15 +0300

    add hash table h

---
+++ headers/classes/HashTable.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <SimCard.h>
+#include <SimCardHash.h>
+
+class HashTable{
+    private:
+        HashSegment** hashTable = new HashSegment* [100];
+        int start_element_count = 100;
+    public:
+        HashTable();
+
+        int doubleHash(std::string, int, int);
+        void showTable();
+        void handleTableOverflow();
+        int countOccupiedCells();
+        void deleteElemen(long long sim_card_number);
+        void addElem(HashSegment new_sim_card);
+        HashSegment* findSimCardByNumber(long long sim_card_number);
+        HashSegment* findSimCardByTariff(std::string sim_card_tariff);
+};
